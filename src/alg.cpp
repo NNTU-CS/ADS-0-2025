ff21d76c65a4ab27bcdf121ff14d0b851aa79ba0
Axeber0 <axeber.yt@gmail.com>
2025-02-14 21:39:04 +0300

    U alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,27 @@
 
 int gcd(int x, int y) {
 	
-	int nod = 0;
-	while (!nod) {
-		if (x > y) {
-			for (int i = y; i; i--) {
-				if (((y % i) == 0) && ((x % i) == 0)) {
-					nod = i;
-					break;
-				}
-			}
-		}
-		if (x < y) {
-			for (int i = x; i; i--) {
-				if (((y % i) == 0) && ((x % i) == 0)) {
-					nod = i;
-					break;
-				}
-			}
-		}
-		if (x == y) {
-			nod = x;
+  int nod = 0;
+  while (!nod) {
+    if (x > y) {
+      for (int i = y; i; i--) {
+        if (((y % i) == 0) && ((x % i) == 0)) {
+          nod = i;
+          break;
         }
-	}
-	return nod;
+      }
+    }
+    if (x < y) {
+      for (int i = x; i; i--) {
+        if (((y % i) == 0) && ((x % i) == 0)) {
+          nod = i;
+          break;
+        }
+      }
+    }
+    if (x == y) {
+      nod = x;
+    }
+  }
+  return nod;
 }
